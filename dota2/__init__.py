eb5a561e2187102b22b27c8473255360f61c0535
thekid42 <thekid4287@gmail.com>
2013-03-03 00:47:17 -0600

    Set up standard Python "project" layout, and added the LGPLv3 license for the time being.

---
+++ dota2/__init__.py
@@ -0,0 +1,4 @@
+__title__ = 'dota2'
+__version__ = '0.1dev'
+__license__ = 'LGPLv3'
+import dota
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Dota 2 Builder',
+    version='0.1dev',
+    packages=['dota2',],
+    license='LGPLv3',
+    long_description=open('README.txt').read(),
+)
